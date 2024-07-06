a5dd63a5947e5e7bc852eabf72c69bf608821edc
harvanchik <66141166+harvanchik@users.noreply.github.com>
2024-07-06 11:12:38 -0700

    fixed size of navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <header class="fixed inset-x-0 top-0 z-10 border-b border-[#694E41]/25 shadow-sm">
-	<nav class="mx-auto flex h-16 items-center justify-between bg-[#faeee5] px-32">
-		<a href="./">
-			<img src="/src/lib/logo2.png" alt="logo" class="h-24" />
+	<nav class="mx-auto flex h-32 items-center justify-between bg-[#faeee5] px-48">
+		<a href="./" class="select-none">
+			<img src="/src/lib/logo.webp" alt="logo" class="h-36" />
 		</a>
 		<!-- about and blog buttons on right side -->
-		<div class="font-cormorant mr-4 text-lg">
+		<div class="font-cormorant mr-4 text-3xl">
 			<a href="/about" class="mr-4 text-[#694E41]">about</a>
 			<a href="/blog" class="mr-4 text-[#694E41]">blog</a>
 			<a href="/subscribe" class="text-[#694E41]">subscribe</a>
