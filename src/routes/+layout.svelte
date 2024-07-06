004aaf58cb4089cc2b42edb835307dab596b81f7
harvanchik <66141166+harvanchik@users.noreply.github.com>
2024-07-06 11:01:12 -0700

    created nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,18 @@
 	import '../app.css';
 </script>
 
+<header class="fixed inset-x-0 top-0 z-10 border-b border-[#694E41]/25 shadow-sm">
+	<nav class="mx-auto flex h-16 items-center justify-between bg-[#faeee5] px-32">
+		<a href="./">
+			<img src="/src/lib/logo2.png" alt="logo" class="h-24" />
+		</a>
+		<!-- about and blog buttons on right side -->
+		<div class="font-cormorant mr-4 text-lg">
+			<a href="/about" class="mr-4 text-[#694E41]">about</a>
+			<a href="/blog" class="mr-4 text-[#694E41]">blog</a>
+			<a href="/subscribe" class="text-[#694E41]">subscribe</a>
+		</div>
+	</nav>
+</header>
+
 <slot></slot>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,11 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				cormorant: ['Cormorant Garamond', 'serif']
+			}
+		}
 	},
 
 	plugins: [require('@tailwindcss/typography')]
