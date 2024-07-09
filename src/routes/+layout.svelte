93bbfda3888bc4997ad1a6748d15f2d579bcf933
harvanchik <66141166+harvanchik@users.noreply.github.com>
2024-07-09 06:59:48 -0700

    made nav smaller

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,16 +7,16 @@
 <header class="fixed inset-x-0 top-0 z-10">
 	<!-- START: Navigation Bar -->
 	<nav
-		class="mx-auto flex h-28 items-center justify-between border-b border-[#694E41]/25 bg-[#faeee5] px-56 shadow-sm"
+		class="mx-auto flex h-24 items-center justify-between border-b border-[#694E41]/25 bg-[#faeee5] px-56 shadow-sm"
 	>
 		<!-- START: Logo -->
 		<a href="./" class="select-none">
-			<img src={logo} alt="logo" class="h-[5.25rem]" />
+			<img src={logo} alt="logo" class="h-16" />
 		</a>
 		<!-- END: Logo -->
 
 		<!-- START: Nav Buttons -->
-		<div class="flex flex-row space-x-10 font-cormorant text-3xl text-[#694E41]">
+		<div class="flex flex-row space-x-10 font-cormorant text-2xl text-[#694E41]">
 			<a href="/about" class="hover:text-[#9e7a69]">about</a>
 			<a href="/blog" class="hover:text-[#9e7a69]">blog</a>
 			<a href="/subscribe" class="hover:text-[#9e7a69]">subscribe</a>
